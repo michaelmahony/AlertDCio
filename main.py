0f4dfd62d02ba81edf6e4ce86bf90b072f3986ff
Michael Mahony <michael.mahony@umbc.edu>
2016-05-30 16:28:43 -0400

    More Procfile changes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     # msg = twilio.twiml.Body()
-    msg = twilio.twiml.Body.body
+    msg = twilio.twiml.Sms.body
     resp = twilio.twiml.Response()
     resp.message(msg)
     return str(resp)
