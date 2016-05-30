72537c6d1183a8b807f1b097ce77b626e60bd092
Michael Mahony <michael.mahony@umbc.edu>
2016-05-30 15:57:49 -0400

    More Procfile changes

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
 
     resp = twilio.twiml.Response()
-    resp.message("Text")
+    resp.message(resp.body)
     return str(resp)
 
 if __name__ == "__main__":
