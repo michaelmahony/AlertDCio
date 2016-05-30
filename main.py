cdd6f9634c0d1d457708a119050c7eea16a508dd
Michael Mahony <michael.mahony@umbc.edu>
2016-05-30 16:08:08 -0400

    More Procfile changes

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
-    msg = twilio.twiml.Body()
+    # msg = twilio.twiml.Body()
     resp = twilio.twiml.Response()
-    resp.message(msg)
+    resp.message("This is a test")
     return str(resp)
 
 if __name__ == "__main__":
