78089f7c94098401a395b7fb3ece6bb4369d9e63
Michael Mahony <michael.mahony@umbc.edu>
2016-05-30 15:40:56 -0400

    More Procfile changes

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def hello_monkey():
 if __name__ == "__main__":
     app.run(debug=True)
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-
-api = tweepy.API(auth)
-api.update_status('')
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+#
+# api = tweepy.API(auth)
+# api.update_status('')
