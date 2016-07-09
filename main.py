769434cc2a6b672b07ce237c7a67e3723a0f1808
michaelmahony <michael.mahony@umbc.edu>
2016-07-09 00:52:57 -0400

    Added user faceing pages for individual tweets and tweet lists.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+
+from pprint import pprint
 
 import html
 import tweepy
@@ -13,6 +16,7 @@ import bs4
 import shelve
 import time
 import datetime
+import pytz
 import BaseConvert
 
 # C:\Users\User1\TweetDC\Scripts\Activate
@@ -27,12 +31,48 @@ ACCESS_SECRET = "TF6UuUst6gV02IYQ23oQPiDGecfmoj5Y0ucYXtZ9K39pf"
 
 app = Flask(__name__)
 
+app.secret_key = 'jsfajgpoiahgpioand;lmnspofihwpifha[oksdfjaposihfPIUFHPOIKJfpaoshf'
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://aycfvofyykjlpb:jcKi1FKYzalEXc2kO6xM3e2S4w@ec2-54-235-125-38.compute-1.amazonaws.com:5432/dcnp19lrmuogfm'
 db = SQLAlchemy(app)
 
-# Define database models
+# Define database modelst
+# DB model for flask-security
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')),
+                       )
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+# Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(email='mmahony1@umbc.edu', password='axega137004')
+#     db.session.commit()
+
+
+# DB model for the tweets
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     long_text = db.Column(db.String(5000))
@@ -43,7 +83,7 @@ class Tweet(db.Model):
     def __init__(self, long_text, short_text):
         self.long_text = long_text
         self.short_text = short_text
-        self.date_time = datetime.datetime.now()
+        self.date_time = datetime.datetime.now(pytz.timezone("America/New_York"))
         self.base62id = BaseConvert.encode(self.id, "123456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ");
 
     def __repr__(self):
@@ -53,7 +93,8 @@ class Tweet(db.Model):
 admin = Admin(app, template_mode='bootstrap3')
 admin.add_view(ModelView(Tweet, db.session))
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/update_tweets_2016", methods=['GET', 'POST'])
+@login_required
 def tweet():
 
     print ('Called')
@@ -91,11 +132,11 @@ def tweet():
     response = ""
 
     for index, long_entry in enumerate(parsed_list):
-        # limit the long entry to 1000 chars
-        long_entry = long_entry[0:999]
+        # limit the long entry to 5000 chars
+        long_entry = long_entry[0:4999]
         # Has the tweet already been sent?
 
-        short_entry = long_entry[0:139]
+        short_entry = long_entry[0:116]
         missing = Tweet.query.filter_by(short_text=short_entry).first()
         if missing is not None:
             # Already tweeted, do nothing
@@ -103,6 +144,27 @@ def tweet():
 
         else:
             try:
+                # Add the new tweet to the database
+                tweet = Tweet(long_entry, short_entry)
+                db.session.add(tweet)
+
+                # Because the ID is not generated until the entry is inserted, we must edit the entry to add
+                #   the base62id
+                last = db.session.query(Tweet).order_by(Tweet.id.desc()).first()
+                base62id = BaseConvert.encode(last.id, "123456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ")
+                last.base62id = base62id
+
+                # An unfortunate consequence of the ID generation scheme is that the insertion needs to take place
+                #   before we can generate the base62id, and thus before the tweet can be issued.
+                #   This makes me uneasy given the risk of the tweet failing.
+
+                # Save the new database state
+                db.session.commit()
+
+                alert_url = "alertdc.io/t/" + base62id
+                short_entry += alert_url
+
+                # Send tweet
                 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
                 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
                 api = tweepy.API(auth)
@@ -113,15 +175,10 @@ def tweet():
                 time.sleep(2)
             except:
                 pass
-            # Add the new tweet to the database
-            tweet = Tweet(long_entry, short_entry)
-            db.session.add(tweet)
-            last = db.session.query(Tweet).order_by(Tweet.id.desc()).first()
-            last.base62id = BaseConvert.encode(last.id, "123456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ")
 
 
-    # Save the new database state
-    db.session.commit()
+
+
 
     response += "\n"
 
@@ -132,26 +189,41 @@ def tweet():
 
     return response
 
+@app.route("/", methods=['GET', 'POST'])
+def recentTweets():
+    query = db.session.query(Tweet).order_by(Tweet.id.desc()).limit(10)
+    results_list = [u.__dict__ for u in query.all()]
+
+    return render_template('index.html', tweet_list=results_list)
+
+
+@app.route("/t/<base62id>")
+def tweetView(base62id):
+    # Get the tweet
+    # the_tweet = db.session.query(Tweet).filter_by(base62id=base62id).first()
+    the_tweet = Tweet.query.filter_by(base62id=base62id).first()
+
+    if the_tweet == None:
+        # Alert not found, redirect
+        flash('Alert not found. Redirecting.') # Doesn't work??
+        return redirect('/')
+
+    tweet_detail = {
+        'date_time' : the_tweet.date_time,
+        'long_text' : the_tweet.long_text,
+        'base62id' : the_tweet.base62id,
+    }
+
+    print(tweet_detail)
+
+    return render_template('tweet.html', tweet=tweet_detail)
 
 
 
 
-# def scrape():
-    # response = requests.get('http://hsema.dc.gov/node/848452')
-    #
-    # soup = bs4.BeautifulSoup(response.text)
-    # msgs = soup.select('table')
-    #
-    # print(msgs)
-    # print("Hello")
+
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-# auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-#
-# api = tweepy.API(auth)
-# api.update_status('')
+    app.run(debug=False)
